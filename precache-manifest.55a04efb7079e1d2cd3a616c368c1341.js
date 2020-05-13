6c6dd7e90c28e6a5bc375bf446c0e82176a3b2e9
Akash Chaudhary <58812239+cyberlord-007@users.noreply.github.com>
2020-05-14 00:06:28 +0530

    Updates

---
+++ precache-manifest.55a04efb7079e1d2cd3a616c368c1341.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a766e97ed255bb8d332bd2649eb9859",
+    "url": "/Todoey-react-app/index.html"
+  },
+  {
+    "revision": "316460b70d1c40f1ea33",
+    "url": "/Todoey-react-app/static/css/main.a4a5bd43.chunk.css"
+  },
+  {
+    "revision": "883948ccfe6a65738ac9",
+    "url": "/Todoey-react-app/static/js/2.699e9d9e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Todoey-react-app/static/js/2.699e9d9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "316460b70d1c40f1ea33",
+    "url": "/Todoey-react-app/static/js/main.9d08dbba.chunk.js"
+  },
+  {
+    "revision": "8e038c6827ad6bebca44",
+    "url": "/Todoey-react-app/static/js/runtime-main.0e996ad9.js"
+  }
+]);
